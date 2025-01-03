12820e98e567d38fb2aab7c3dd98cc416dc6c759
AdhyanRaghav <adhyanraghav@gmail.com>
2025-01-03 23:48:36 +0530

    Create App.java

---
+++ build/classes/javasemesterproject/App.java
@@ -0,0 +1,10 @@
+package com.elearning;
+
+import com.elearning.views.LoginView;
+
+public class App {
+    public static void main(String[] args) {
+        // Launch the Login GUI
+        new LoginView();
+    }
+}
